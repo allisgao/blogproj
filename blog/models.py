703433150cd2661bba96470e5431d1016bb45f8b
George Gao <gaojz017@163.com>
2018-12-07 14:16:50 +0800

    create db models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+"""
+Category: 分类
+Tag： 标签
+Post： 文章
+"""
+
+class Catagory(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+
+
+class Post(models.Model):
+    # 标题
+    title = models.CharField(max_length=100)
+    # 正文
+    body = models.TextField()
+    # 时间，创建时间和修改时间
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    # 文章摘要
+    excerpt = models.CharField(max_length=200, blank=True)
+    # 分类和标签。这里，规定一篇文章只能有一个分类，但可以有多个标签。
+    ## 分类。一篇文章只有一个分类，一个分类下可以有多篇文章
+    category = models.ForeignKey(Categroy)
+    ## 标签。一篇文章可以有多个分类，一个分类下也可以有多篇文章
+    tags = models.ManyToManyField(Tag, blank=True)
+    # 作者。
+    author = models.ForeignKey(User)
+
+
+
+
+
