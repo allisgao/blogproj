1686579428998c2f61dc0abc7efb1c7ccd4f9953
George Gao <gaojz017@163.com>
2018-12-07 17:51:41 +0800

    chage templates's path

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'blogproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
