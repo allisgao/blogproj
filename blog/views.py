2ef21365091bd4e2dba6bfb5487635c87506e173
George Gao <gaojz017@163.com>
2018-12-07 18:01:38 +0800

    a render view page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<center>欢迎访问我的博客首页^_^</center>")
+    # return HttpResponse("<center>欢迎访问我的博客首页^_^</center>")
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
