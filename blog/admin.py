39c87e2722030f3888db980158880229f15884e7
George Gao <gaojz017@163.com>
2018-12-07 19:23:45 +0800

    publish posts using django admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+# 让后台显示更多的信息
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+admin.site.register(Post, PostAdmin)
+# admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
+
+
