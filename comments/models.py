2bc569b077a6cb09a8ee0ed48a2988ff211d6725
George Gao <gaojz017@163.com>
2018-12-08 11:27:27 +0800

    create data models of comments

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    # 评论是关联到某篇文章（Post）的，由于一个评论只能属于一篇文章，一篇文章可以有多个评论，是一对多的关系，因此这里我们使用了 ForeignKey。
+    """
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    url = models.URLField(blank=True)
+    text = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    post = models.ForeignKey('blog.Post')
+
+    def __str__(self):
+        return self.text[:20]
