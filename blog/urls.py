3abb74d5aee9b7f66f18e820ef7bed10307493f8
George Gao <gaojz017@163.com>
2018-12-08 10:55:42 +0800

    add archive

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,11 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'blog'   #  通过 app_name='blog' 告诉 Django 这个 urls.py 模块是属于 blog 应用的，这种技术叫做视图函数命名空间。
+app_name = 'blog'   # 通过 app_name='blog' 告诉 Django 这个 urls.py 模块是属于 blog 应用的，这种技术叫做视图函数命名空间。
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail')
+    url(r'^$', views.index, name='index'),  # 首页
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),    # 详情页
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', \
+        views.archives, name='archives'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,24 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 import markdown
 
+
 def index(request):
+    """
+    # 首页
+    :param request:
+    :return:
+    """
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list,})
 
+
 def detail(request, pk):
+    """
+    # 详细页，即点开可以查看博客内容
+    :param request:
+    :param pk:
+    :return:
+    """
     post = get_object_or_404(Post, pk=pk)
     # 支持markdown
     post.body = markdown.markdown(post.body,
@@ -19,3 +32,18 @@ def detail(request, pk):
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
 
+
+def archives(request, year, month):
+    """
+    归档
+    :param request:
+    :param year:
+    :param month:
+    :return:
+    """
+    post_list = Post.objects.filter(created_time__year=year,
+                                    # Python 中类实例调用属性的方法通常是 created_time.year，\
+                                    # 但是由于这里作为函数的参数列表，所以 Django 要求我们把点替换成了两个下划线，即 created_time__year。
+                                    created_time__month=month,
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
