21a81e1df52666ca3152e68e22326182ea9ece56
George Gao <gaojz017@163.com>
2018-12-12 16:08:49 +0800

    repair a bug.Now, the recent posts can order in recent-release time.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -15,7 +15,7 @@ def get_recent_posts(num=5):
     :param num:
     :return:
     """
-    return Post.objects.all().order_by('created_time')[:num]
+    return Post.objects.all().order_by('-created_time')[:num]
 
 @register.simple_tag
 def archives():
@@ -36,5 +36,4 @@ def get_categories():
 @register.simple_tag
 def get_tags():
     return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
-
 
